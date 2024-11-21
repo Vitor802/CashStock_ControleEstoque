4d65b4e69c2a5bf0b0c1e813c5aca463ae471e4d
Shotsu1 <802.441@alunos.unigran.br>
2024-11-20 20:48:11 -0400

    Initial commit

---
+++ src/test/java/br/com/CashStock_ControleEstoque/CashStockControleEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.CashStock_ControleEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashStockControleEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
